7587670e82913744df5fd898d92e6d48cd8fe516
Mujee <mujee2@gmail.com>
2022-09-06 09:59:44 +0800

    Creation of Scaffold and Custom Row composabals

--- app/src/main/java/com/mujapps/moviescompose/MainActivity.kt
+++ app/src/main/java/com/mujapps/moviescompose/MainActivity.kt
@@ -3,41 +3,100 @@ package com.mujapps.moviescompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mujapps.moviescompose.ui.theme.MoviesComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MoviesComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MyMovieContent()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(content: @Composable () -> Unit) {
+    MoviesComposeTheme {
+        //Scaffold is the Material design basement
+        Scaffold(topBar = {
+            TopAppBar(
+                backgroundColor = Color.Magenta,
+                elevation = 4.dp
+            ) {
+                Text(text = "Movies")
+            }
+        }) {
+            it.calculateBottomPadding()
+            content()
+        }
+    }
+}
+
+@Composable
+fun MyMovieContent(
+    movieList: List<String> = listOf(
+        "Avatar", "300", "Hell Boy", "Godzilla", "Iron Man", "Interstellar",
+        "Anaconda", "Blood Diamond", "The Dune"
+    )
+) {
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn {
+            items(items = movieList) {
+                MovieRow(it)
+            }
+        }
+    }
+}
+
+@Composable
+fun MovieRow(movie: String) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(100.dp),
+        shape = RoundedCornerShape(corner = CornerSize(12.dp)),
+        elevation = 4.dp
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .size(72.dp),
+                shape = RectangleShape,
+                elevation = 4.dp
+            ) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+            }
+            Text(text = movie)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MoviesComposeTheme {
-        Greeting("Android")
+    MyApp {
+        MyMovieContent()
     }
 }
